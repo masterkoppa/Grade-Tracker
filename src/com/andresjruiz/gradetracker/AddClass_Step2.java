aaad68c59e3e72f23f570b29cd855381106c8edc
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-03-16 15:05:33 -0400

    Fix rotations in Step2
    Make it so that the rotation of the device wouldn't reset
    the data entered by the user. Lots of changes had to be made
    and will have to be made to get every other activity to work
    in a similar fashion to avoid any problems with rotation.

--- src/com/andresjruiz/gradetracker/AddClass_Step2.java
+++ src/com/andresjruiz/gradetracker/AddClass_Step2.java
@@ -1,5 +1,6 @@
 package com.andresjruiz.gradetracker;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -7,11 +8,14 @@ import android.app.Dialog;
 import android.app.ListActivity;
 import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -25,64 +29,58 @@ public class AddClass_Step2 extends ListActivity {
 	private SimpleCursorAdapter adapter;
 	private Map<String, Integer> categories;
 	private String[] listMappings;
+	/**
+     * <ul>
+     * <li>The first element has to be some non-related view to put numbers in</li>
+     * <li>The second element must be where the category name will be</li>
+     * <li>The third element must be where the percentage will be shown</li>
+     * </ul>
+     */
+    private int[] viewMapping;
 	
 	/** Called when the activity is first created. */
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
+    	
+    	gatherBundleInformation();
     	
     	//Remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
     	
-        setContentView(R.layout.add_class_categories);
+        setContentView(R.layout.add_class_categories);      
         
-        //this.getListView().addFooterView(findViewById(R.id.border_buttons));
-        
-        //Get the information from the past screen
-        Bundle info = this.getIntent().getExtras();
-        
-        ClassName = info.getString("ClassName");
-        ProfName = info.getString("ProfName");
-        
-        //Set the title for this screen
-        
-        TextView title = ((TextView)findViewById(R.id.categoriesTitle));
-        
-        title.append(": " + ClassName);
-        
-        Button next = (Button) findViewById(R.id.next_button);
-        
-        next.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-			}
-		});
-        
-        Button back = (Button) findViewById(R.id.back_button);
-        
-        back.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
+        setTitleText();
         
         //Defines the keys that will be associated with each object
         listMappings = new String[]{"_id", "Name", "Percent"};
         //Defines the resources linked to the keys
-        int[] viewMapping = {R.id.textView1, R.id.class_name, R.id.grade_display };
-        
+        viewMapping = new int[]{R.id.textView1, R.id.class_name, R.id.grade_display };
         data = new MatrixCursor(listMappings);
-        data.addRow(new String[]{"0", "Exams", "50%"});
-        data.addRow(new String[]{"1", "Homework", "50%"});
+        try{
+        	categories = ((Map<String, Integer>) getLastNonConfigurationInstance());
+        }catch(ClassCastException e){
+        	Log.e("ERROR_CAUGHT", e.getMessage());
+        }
         
-        this.categories = new TreeMap<String, Integer>();
-        
-        categories.put("Exams", 50);
-        categories.put("Homework", 50);
+        if(categories == null){
+	        data.addRow(new String[]{"0", "Exams", "50%"});
+	        data.addRow(new String[]{"1", "Homework", "50%"});
+	        
+	        this.categories = new TreeMap<String, Integer>();
+	        
+	        categories.put("Exams", 50);
+	        categories.put("Homework", 50);
+        } else{
+        	int i = 0;
+    		for(String Cat : categories.keySet()){
+    			int percent = categories.get(Cat);
+    			data.addRow(new String[]{Integer.toString(i), Cat, percent + "%"});
+    			i++;
+    		}
+        	
+        }
 
         //ListAdapter adapter = new SimpleAdapter(this, listData, R.layout.main_list_item, listMappings, viewMapping);
         adapter = new SimpleCursorAdapter(this, R.layout.category_edit_item, data, listMappings, viewMapping);
@@ -90,22 +88,53 @@ public class AddClass_Step2 extends ListActivity {
         //Fill in the list data
         setListAdapter(adapter);
         
-        
-        //Deal with adding categories
-        
-        Button button = (Button) findViewById(R.id.addCategoryButton);
-        
-        button.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				//showGradeSelector(10, 0);
-				addCategory();
+        //List Item Listener
+        this.getListView().setOnItemClickListener(new OnItemClickListener(){
+
+        	@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+			        int position, long id) {
+        		String catName = (String) ((TextView) view.findViewById(R.id.class_name)).getText();
+        		String grade = (String) ((TextView) view.findViewById(R.id.grade_display)).getText();
+        		//Remove the % sign at the end
+        		grade = grade.substring(0, grade.length()-1);
+        		
+        		int weight = Integer.parseInt(grade);
+        		
+        		showGradeSelector(catName, weight, (int)id);
 			}
-		});
+        	
+        });
+        
+        setButtonListeners();
     }
     
-    private void showGradeSelector(int startPercent, int catID){
+    @Override
+    public Object onRetainNonConfigurationInstance(){
+		return categories;
+    	
+    }
+
+	private void gatherBundleInformation(){
+    	//Get the information from the past screen
+        Bundle info = this.getIntent().getExtras();
+        
+        ClassName = info.getString("ClassName");
+        ProfName = info.getString("ProfName");
+    }
+    
+    private void setTitleText(){
+    	//Set the title for this screen
+        
+        TextView title = ((TextView)findViewById(R.id.categoriesTitle));
+        title.append(": " + ClassName);
+        
+        if(ProfName.length() >= 1){
+        	title.append(" By: " + ProfName);
+        }
+    }
+    
+    private void showGradeSelector(String Category, int startPercent, int catID){
     	final Dialog dialog = new Dialog(this);
     	
     	dialog.setContentView(R.layout.custom_seek_dialog);
@@ -135,7 +164,6 @@ public class AddClass_Step2 extends ListActivity {
     	
     	seek.setProgress(startPercent);
     	
-    	
     	//Handle the confirm button
     	Button confirmButton = (Button)(dialog.findViewById(R.id.confirm));
     	confirmButton.setOnClickListener(new OnClickListener(){
@@ -148,6 +176,44 @@ public class AddClass_Step2 extends ListActivity {
     	});
     	
     	dialog.show();
+    }
+    
+    /**
+     * Handles all the logistics of getting the buttons working for this activity
+     */
+    private void setButtonListeners(){
+    	//The next button
+    	Button next = (Button) findViewById(R.id.next_button);
+        
+        next.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+        
+        //The back button
+        Button back = (Button) findViewById(R.id.back_button);
+        
+        back.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+        
+        //The add category button
+        Button button = (Button) findViewById(R.id.addCategoryButton);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				addCategory();
+			}
+		});
     }
     
     /**
